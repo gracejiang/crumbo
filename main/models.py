f5117f125ab3db6bfcecdb3b42715e6a1d61179a
cx-lu <cxlu@wharton.upenn.edu>
2021-11-16 20:36:41 -0500

    Fixed FoodPlace model

--- main/models.py
+++ main/models.py
@@ -5,24 +5,40 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class FoodPlace(models.Model):
 
-    class Type(models.TextChoices):
-        AMERICAN = 'AM', _('American')
-        MEXICAN = 'MX', _('Mexican')
-        CHINESE = 'CH', _('Chinese')
-        KOREAN = 'KR', _('Korean')
-        JAPANESE = 'JP', _('Japanese')
-        ITALIAN = 'IT', _('Italian')
-        VIETNAMESE = "VN", _('Vietnamese')
-        INDIAN = 'IN', _('Indian')
-        GREEK = 'GR', _('Greek')
-        FRENCH = 'FR', _('French')
-        MEDITERRANEAN = "MD", _('Mediterranean')
-        CAFE = 'CF', _('Cafe')
-        FAST_FOOD = 'FF', _('Fast Food')
+    AMERICAN = 'AM'
+    MEXICAN = 'MX'
+    CHINESE = 'CH'
+    KOREAN = 'KR'
+    JAPANESE = 'JP'
+    ITALIAN = 'IT'
+    VIETNAMESE = "VN"
+    INDIAN = 'IN'
+    GREEK = 'GR'
+    FRENCH = 'FR'
+    MEDITERRANEAN = "MD"
+    CAFE = 'CF'
+    FAST_FOOD = 'FF'
+
+    TYPE_CHOICES = [
+        (AMERICAN, 'American'),
+        (MEXICAN, 'Mexican'),
+        (CHINESE, 'Chinese'),
+        (KOREAN, 'Korean'),
+        (JAPANESE, 'Japanese'),
+        (ITALIAN, 'Italian'),
+        (VIETNAMESE, 'Vietnamese'),
+        (INDIAN, 'Indian'),
+        (GREEK, 'Greek'),
+        (FRENCH, 'French'),
+        (MEDITERRANEAN, 'Mediterranean'),
+        (CAFE, 'Cafe'),
+        (FAST_FOOD, 'Fast Food')
+    ]
 
     name = models.CharField(max_length=200, default='')
     type = models.CharField(
-        max_length=2, choices=Type.choices, default=Type.AMERICAN)
+        max_length=200,
+        choices=TYPE_CHOICES, default=AMERICAN)
     location = models.CharField(max_length=200, blank=True, default='')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -32,4 +48,4 @@ class FoodPlace(models.Model):
         default=3,
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     description = models.TextField(blank=True, default='')
-    photos = models.JSONField()  # Takes in JSON array of URLs
+    photos = models.TextField(blank=True, default='')
