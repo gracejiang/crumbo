7c5d59cdbfe7846f6fd62a24102741d4d44a8dc7
Grace Jiang <gracjiang@gmail.com>
2021-11-18 18:53:20 -0500

    filter by user + search feature working! :)

--- main/views.py
+++ main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from main.models import FoodPlace
 
 def home_view(request):
     return render(request, 'home.html' )
 
 def explore_view(request):
-    
-    foodplaces = FoodPlace.objects.order_by('-created_on')
+    searchText = (request.POST['searchText'] if ('searchText' in request.POST) else "")
+    filterUser = (request.POST['filterUser'] if ('filterUser' in request.POST) else "")
+
+    if filterUser == "all" or filterUser == "":
+        foodplaces = FoodPlace.objects.filter(name__contains=searchText).order_by('-created_on')
+    else:
+        user = User.objects.get(username=filterUser)
+        foodplaces = FoodPlace.objects.filter(name__contains=searchText).filter(author=user).order_by('-created_on')
+
     return render(request, 'explore.html', { 'foodplaces' : foodplaces } )
 
 def grac_view(request):
